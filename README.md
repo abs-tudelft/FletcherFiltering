1f96701b34e1c5b1f1eda3c06fd54023e5528a9a
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-09-25 15:04:13 +0200

    Added some README.md stuff.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # FletcherFiltering
 SQL to HLS compiler that generates hardware filters and transformations for [Flechter](https://github.com/abs-tudelft/fletcher) streams.
+
+
+If you want to use the compiler from python, you can call the compile function directly. Here is an excerpt from [\_\_main\_\_.py](src/fletcherfiltering/codegen/__main__.py#L42-L69):
+```py
+if os.path.isfile(args.input_schema):
+    if os.access(args.input_schema, os.R_OK):
+        in_schema = pa.read_schema(args.input_schema)
+    else:
+        raise SchemaFileNotReadable()
+else:
+    raise FileNotFoundError(args.input_schema)
+
+if os.path.isfile(args.output_schema):
+    if os.access(args.output_schema, os.R_OK):
+        out_schema = pa.read_schema(args.output_schema)
+    else:
+        raise SchemaFileNotReadable()
+else:
+    raise FileNotFoundError(args.output_schema)
+
+if os.path.isdir(args.output_dir):
+    if not os.access(args.output_dir, os.W_OK):
+        raise OutputDirIsNotWritable()
+else:
+    raise NotADirectoryError(args.output_dir)
+
+compiler = Compiler(in_schema, out_schema)
+
+if len(args.query_str) < settings.MINIMAL_QUERY_LENGTH:
+    raise QueryTooShort()
+
+compiler(query_str=args.query_str, query_name=args.query_name, output_dir=Path(args.output_dir), meta_length_source=args.meta_length_source)
+```
+Here `meta_length_source` is the name of the column that is the source for the generated kernel length attribute in it's metadata. By default this is 'pkid'.
+
+If you want to use the compiler without using the parser, you can use it like this the following excerpt from [compiler.py](src/fletcherfiltering/codegen/compiler.py#L121-L123):
+```py
+header_ast, general_ast, header_test_ast, general_test_ast = self.transform(query, current_query_name)
+self.output(header_ast, general_ast, header_test_ast, general_test_ast, output_dir, current_query_name,
+            include_test_system=include_test_system)
+```
+Where `query` is the input `AST`. And self is the `Compiler` object as instantiated in the code above.
--- src/fletcherfiltering/settings.py
+++ src/fletcherfiltering/settings.py
@@ -30,8 +30,8 @@ OVERWRITE_DATA = True
 
 VAR_LENGTH_TYPES = [pa.string()]
 VAR_LENGTH = 255
-PORT_TYPE = 'ap_fifo' # axis would be proper, but it has weird reserved named resulting in TID suffixes. And has longer latency.
-BLOCK_LEVEL_IO_TYPE = 'ap_ctrl_hs' # ap_ctrl_chained if backpressure is required.
+PORT_TYPE = 'ap_fifo'  # axis would be proper, but it has weird reserved named resulting in TID suffixes. And has longer latency.
+BLOCK_LEVEL_IO_TYPE = 'ap_ctrl_hs'  # ap_ctrl_chained if backpressure is required.
 META_PORT_TYPE = 'register'
 PASS_VAR_NAME = '__pass_record'
 CURRENT_RECORD_VAR_NAME = 'current_record'
@@ -46,11 +46,11 @@ OUTPUT_NAME = 'ff_out'
 META_NAME = 'meta'
 INPORT_PREFIX = 'i_'
 OUTPORT_PREFIX = 'o_'
-STRING_BASE_TYPE = 'f_uint8' #in code
-STRING_BASE_TYPE_TEST = 'char' #in test bench
+STRING_BASE_TYPE = 'f_uint8'  # in code
+STRING_BASE_TYPE_TEST = 'char'  # in test bench
 LENGTH_TYPE = pa.int32()
 VALID_TYPE = pa.bool_()
-PART_NAME = 'xcku060-ffva1156-2-e' # 'xa7a12tcsg325-1q' 40/40/16k/8k or alveo: xcu200-fsgd2104-2-e or zynq 700: xc7z020clg400-1 or nimbix: xcku060-ffva1156-2-e
+PART_NAME = 'xcku060-ffva1156-2-e'  # 'xa7a12tcsg325-1q' 40/40/16k/8k or alveo: xcu200-fsgd2104-2-e or zynq 700: xc7z020clg400-1 or nimbix: xcku060-ffva1156-2-e
 
 if 'FLETCHER_DIR' in os.environ:
     FLETCHER_DIR = Path(os.environ['FLETCHER_DIR'])
@@ -67,9 +67,9 @@ BUILD_CONFIG = 'Release'
 CLEAN_WORKDIR = True
 SWALLOW_OUTPUT = False
 
-REL_TOL_FLOAT16 = 1e-2 # digits for half float: 3.31
-REL_TOL_FLOAT32 = 1e-5 # digits for single float: 7.22
-REL_TOL_FLOAT64 = 1e-9 # digits for double float: 15.95
+REL_TOL_FLOAT16 = 1e-2  # digits for half float: 3.31
+REL_TOL_FLOAT32 = 1e-5  # digits for single float: 7.22
+REL_TOL_FLOAT64 = 1e-9  # digits for double float: 15.95
 
 FLOAT16_MAX = 65504.0
 FLOAT16_MIN = 0.000061035
@@ -77,10 +77,10 @@ FLOAT16_MIN = 0.000061035
 NULLPROBABILITY = 0.10
 EMPTYSTRINGPROBABILITY = 0.01
 
-MYSQL_USER='fletcherfiltering'
-MYSQL_PASSWORD='pfUcFN4S9Qq7X6NDBMHk'
-MYSQL_HOST='127.0.0.1'
-MYSQL_DATABASE='fletcherfiltering'
+MYSQL_USER = 'fletcherfiltering'
+MYSQL_PASSWORD = 'pfUcFN4S9Qq7X6NDBMHk'
+MYSQL_HOST = '127.0.0.1'
+MYSQL_DATABASE = 'fletcherfiltering'
 
 TEST_PARTS = []
 CMAKE_GENERATOR = 'Ninja'
@@ -88,14 +88,14 @@ VIVADO_DIR = Path('/opt/Xilinx/Vivado/2018.1')
 VIVADO_BIN_DIR = VIVADO_DIR / 'bin'
 VIVADO_EXEC = 'vivado'
 VIVADO_HLS_EXEC = 'vivado_hls'
-HLS_INCLUDE_PATH = [VIVADO_DIR / 'include'] # For build C++ code outside of Vivado HLS
+HLS_INCLUDE_PATH = [VIVADO_DIR / 'include']  # For build C++ code outside of Vivado HLS
 HLS_LINK_PATH = [VIVADO_DIR / Path('linux64/tools/fpo_v7_0')]
 HLS_LIBS = ['gmp', 'mpfr', 'Ip_floating_point_v7_0_bitacc_cmodel']
 
 if platform.system() == "Windows":
     TEST_PARTS = ['sql', 'vivado']
     MYSQL_HOST = '10.211.55.2'
-    #CMAKE_GENERATOR = 'Visual Studio 15 2017 Win64'
+    # CMAKE_GENERATOR = 'Visual Studio 15 2017 Win64'
     VIVADO_DIR = Path('C:/Xilinx/Vivado/2019.1')
     VIVADO_BIN_DIR = VIVADO_DIR / 'bin'
     VIVADO_EXEC = VIVADO_BIN_DIR / 'vivado.bat'
@@ -105,10 +105,10 @@ if platform.system() == "Windows":
     HLS_LIBS = ['libgmp', 'libmpfr', 'libIp_floating_point_v7_0_bitacc_cmodel']
 elif platform.system() == "Darwin":
     TEST_PARTS = []
-    #CMAKE_GENERATOR = 'Ninja'
+    # CMAKE_GENERATOR = 'Ninja'
     VIVADO_DIR = Path('/Users/erwin/Xilinx/Vivado/2019.1')
     VIVADO_BIN_DIR = VIVADO_DIR / 'bin'
-    #VIVADO_HLS_EXEC = VIVADO_BIN_DIR / 'vivado_hls'
+    # VIVADO_HLS_EXEC = VIVADO_BIN_DIR / 'vivado_hls'
     HLS_INCLUDE_PATH = [VIVADO_DIR / 'include']
     HLS_LINK_PATH = []
     HLS_LIBS = ''
